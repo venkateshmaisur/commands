3ef45558ae03a1c0271c517983a6606faefeda07
Pravin Bhagade <pbhagade@hortonworks.com>
2019-08-10 15:01:07 +0530

    Update sssd.sh

--- sssd.sh
+++ sssd.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+## wget https://raw.githubusercontent.com/bhagadepravin/commands/master/sssd.sh && chmod +x sssd.sh && ./sssd.sh
 AD_USER="test1"
 AD_DOMAIN="support.com"
 AD_DC="sme-2012-ad.support.com"
