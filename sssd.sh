4efbb1561f2947a4146f58e03bd102df39440ee5
Pravin Bhagade <pbhagade@hortonworks.com>
2019-08-10 14:48:37 +0530

    Create sssd.sh

---
+++ sssd.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+
+AD_USER="test1"
+AD_DOMAIN="support.com"
+AD_DC="sme-2012-ad.support.com"
+AD_ROOT="dc=support,dc=com"
+AD_OU="dc=support,dc=com"
+AD_REALM=${AD_DOMAIN^^}
+
+echo hadoop12345! | kinit test1@SUPPORT.COM
+yum -y install epel-release sssd oddjob-mkhomedir authconfig sssd-krb5 sssd-ad sssd-tools adcli -q
+adcli join -v --domain-controller=${AD_DC} --domain-ou="${AD_OU}" --login-ccache="/tmp/krb5cc_0" --login-user="${AD_USER}" -v --show-details
+sudo tee /etc/sssd/sssd.conf > /dev/null <<EOF
+[sssd]
+## master & data nodes only require nss. Edge nodes require pam.
+services = nss, pam, ssh, autofs, pac
+config_file_version = 2
+domains = ${AD_REALM}
+override_space = _
+[domain/${AD_REALM}]	
+id_provider = ad
+ad_server = ${AD_DC}
+#ad_server = ad01, ad02, ad03
+#ad_backup_server = ad-backup01, 02, 03
+auth_provider = ad
+chpass_provider = ad
+access_provider = ad
+enumerate = False
+krb5_realm = ${AD_REALM}
+ldap_schema = ad
+ldap_id_mapping = True
+cache_credentials = True
+ldap_access_order = expire
+ldap_account_expire_policy = ad
+ldap_force_upper_case_realm = true
+fallback_homedir = /home/%d/%u
+default_shell = /bin/false
+ldap_referrals = false
+[nss]
+memcache_timeout = 3600
+override_shell = /bin/bash
+EOF
+chmod 0600 /etc/sssd/sssd.conf
+service sssd restart
+authconfig --enablesssd --enablesssdauth --enablemkhomedir --enablelocauthorize --update
+chkconfig oddjobd on
+service oddjobd restart
+chkconfig sssd on
+service sssd restart
