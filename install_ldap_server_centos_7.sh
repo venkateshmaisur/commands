4ed45599469ff365bc992238d8f38912dc969ced
Pravin Bhagade <iampravinbhagade@gmail.com>
2022-07-26 12:27:18 +0530

    Update install_ldap_server_centos_7.sh

--- install_ldap_server_centos_7.sh
+++ install_ldap_server_centos_7.sh
@@ -6,18 +6,19 @@
 
 # Clean packages
 sudo yum clean all
-# Install openldap as service manager
+
+# Install openldap as service manager and php ldap module
 yum -y install openldap-servers openldap-clients openldap-* nss-pam-ldap* php70w-ldap --skip-broken
 
 # If no pass defined do not execute script
 if [[ -z $1 ]]; then
-cat << EOF
+    cat <<EOF
 = = = = = = = = = =
 You must pass a password as first parameter!
 Use the command "slappasswd" to generate a password and pass as first parameter along this script
 = = = = = = = = = =
 EOF
-        exit;
+    exit
 fi
 # If config is not set get new one
 if [ ! -f /var/lib/ldap/DB_CONFIG ]; then
@@ -33,18 +34,18 @@ sudo systemctl enable slapd
 sudo service httpd restart
 
 # Increase search limits
-define(){ IFS='\n' read -r -d '' ${1} || true; }
-define LDAP_CONFIG << EOF
+define() { IFS='\n' read -r -d '' ${1} || true; }
+define LDAP_CONFIG <<EOF
 dn: cn=config
 changetype: modify
 replace: olcSizeLimit
 olcSizeLimit: -1
 EOF
-echo "$LDAP_CONFIG" > increase_limits.ldif
+echo "$LDAP_CONFIG" >increase_limits.ldif
 ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f increase_limits.ldif
 
 # Add root to LDAP Server
-define LDAP_ROOT << EOF
+define LDAP_ROOT <<EOF
 dn: olcDatabase={0}config,cn=config
 changetype: modify
 replace: olcRootPW
@@ -52,24 +53,16 @@ olcRootPW: $1
 EOF
 
 # Move to tmp file because badlt support of stdin on ldap commands
-echo "$LDAP_ROOT" > chrootpw.ldif
+echo "$LDAP_ROOT" >chrootpw.ldif
 sudo ldapadd -Y EXTERNAL -H ldapi:/// -f chrootpw.ldif
 
 # Import basic schemas
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/core.ldif
 sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
 sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/openldap.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/collective.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/corba.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/duaconf.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/java.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/pmi.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/ppolicy.ldif
-sudo ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/dyngroup.ldif
+
 # Add domain to LDAP Server
-define LDAP_DOMAIN << EOF
+define LDAP_DOMAIN <<EOF
 dn: olcDatabase={1}monitor,cn=config
 changetype: modify
 replace: olcAccess
@@ -101,20 +94,16 @@ olcAccess: {2}to * by dn="cn=Manager,dc=pravin,dc=com" write by * read
 EOF
 
 # Move to tmp file because ldap support of stdin on ldap commands
-echo "$LDAP_DOMAIN" > chdomain.ldif
+echo "$LDAP_DOMAIN" >chdomain.ldif
 sudo ldapadd -Y EXTERNAL -H ldapi:/// -f chdomain.ldif
 
-
-
-service slapd restart
-
-define LDAP_BASE_DOMAIN << EOF
+# Add root to LDAP Server
+define LDAP_BASE_DOMAIN <<EOF
 dn: dc=pravin,dc=com
 objectClass: dcObject
 objectClass: organization
 o: pravin.com
 dc: pravin
-
 
 dn: ou=users,dc=pravin,dc=com
 objectClass: organizationalUnit
@@ -128,60 +117,43 @@ ou: groups
 EOF
 
 # Move to tmp file because badlt support of stdin on ldap commands
-echo "$LDAP_BASE_DOMAIN" > basedomain.ldif
+echo "$LDAP_BASE_DOMAIN" >basedomain.ldif
 sudo ldapadd -x -D cn=Manager,dc=pravin,dc=com -W -f basedomain.ldif
 
-define LDAP_FAKE_USER << EOF
-dn: uid=pbhagade,ou=users,dc=pravin,dc=com
-objectClass: top
-objectClass: account
-objectClass: posixAccount
-objectClass: shadowAccount
+define LDAP_FAKE_USER <<EOF
+dn: cn=admin,dc=ldap,dc=dev
+objectClass: inetOrgPerson
+sn: admin
+cn: admin
+title: admin
+uid: 10001
+userPassword: changeit
+displayName: admin 
+
+dn: cn=pbhagade,dc=ldap,dc=dev
+objectClass: inetOrgPerson
+sn: pbhagade
 cn: pbhagade
-uid: pbhagade
-uidNumber: 10001
-gidNumber: 10001
-homeDirectory: /home/pbhagade
-loginShell: /bin/bash
-gecos: pbhagade
-userPassword: Welcome
-shadowLastChange: 0
-shadowMax: 0
-shadowWarning: 0
+title: pbhagade
+uid: 10001
+userPassword: changeit
+displayName: pbhagade 
+
 
 dn: cn=group1,ou=groups,dc=pravin,dc=com
-objectClass: groupOfNames
+objectClass: inetOrgPerson
+sn: Nom
 cn: group1
-description: group1
-member: uid=pbhagade,ou=users,dc=pravin,dc=com
-
-dn: uid=pravin,ou=users,dc=pravin,dc=com
-objectClass: top
-objectClass: account
-objectClass: posixAccount
-objectClass: shadowAccount
-cn: pravin
-uid: pravin
-uidNumber: 10002
-gidNumber: 10002
-homeDirectory: /home/pravin
-loginShell: /bin/bash
-gecos: pravin
-userPassword: Welcome
-shadowLastChange: 0
-shadowMax: 0
-shadowWarning: 0
-
+title: group1
+uid: 98785
+userPassword: changeit
+displayName: group1
 EOF
 
 # ADD Fake user to GROUP LAPLACE
-echo "$LDAP_FAKE_USER" > fakeuser.ldif
+echo "$LDAP_FAKE_USER" >fakeuser.ldif
 sudo ldapadd -x -D cn=Manager,dc=pravin,dc=com -W -f fakeuser.ldif
 
 ### INSTAL LDAPADMINi
 ### FOLLOW HERE http://www.server-world.info/en/note?os=CentOS_7&p=openldap&f=7
-sudo yum --enablerepo=epel -y install phpldapadmin -y
-
-# ldapsearch -h localhost -p 389 -D cn=Manager,dc=pravin,dc=com -w Welcome -b dc=pravin,dc=com
-#
-#
+# sudo yum --enablerepo=epel -y install phpldapadmin -y
